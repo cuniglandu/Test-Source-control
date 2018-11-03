69399769b551057df65bd5f57028622ea85c4fba
Yann Letouzey <yannletouzey@macbook-air.local>
2018-11-03 07:02:28 +0100

    Ajout classe Test

---
+++ Test du Source control/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Test du Source control
+//
+//  Created by Yann Letouzey on 03/11/2018.
+//  Copyright © 2018 Yann Letouzey. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print("Salut les codabeilles")
+    }
+}
